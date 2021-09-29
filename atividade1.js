c788388901107936286f5ff1dfa45046968d586a
Paulo Henrique Dias <34211685+PauloHenriqueDias@users.noreply.github.com>
2021-09-28 21:01:34 -0300

    Update atividade1.js

--- atividade1.js
+++ atividade1.js
@@ -7,32 +7,39 @@ alertar que o cadastro não é permitido pela idade.
 -Enquanto a quantidade de participantes for inferior a 100, permitir cadastro; senão,
  alertar que o cadastro não será permitido por ter excedido o limite.*/
 //----------------------------------------------------------------------------------------//
+
+let readlineSync = require('readline-sync');
+
 var inicio = 0;
 var cadastros = 2;
 while (inicio < cadastros) {
 
 	var primeiroDia = new Date();
     var dataAtual = String(primeiroDia. getDate()). padStart(2 ,'0');//buscando somente o primeiro dia da data
-    var dataEvento = 18;
-    
+    var dataEvento = readlineSync.question("Digite o primeiro dia da data do Evento: ");  
+    var nomeParticipante = readlineSync.question("Digite o nome do Participante : ");
+    var idade = readlineSync.question("Digite a idade: ");
+    var numeroParticipante = readlineSync.question("Digite a quantidade de Participante: ");  
+    var numeroPalestrante = readlineSync.question("Digite a quantidade de Palestrante : ");
+    console.log("----------------------------------------------------");
         if(dataEvento < dataAtual) {//condição para comparar a data atual com a data do evento inicialmente fixa.
-            console.log("**cadastro não realizado**");
+            console.log("**cadastro não realizado, data inválida**");
        
         }else{
             
             console.log("**Cadastro realizado com sucesso!**");
         }
-    var idade = 18;
         if(idade >= 18) {
             console.log("**permitir cadastro, maior de idade!**");
         }else{
             console.log("**cadastro não realizado, menor de idade!**");
         }
-    var numeroParticipante = 101;
-    var numeroPalestrante = 1;
-        if(numeroParticipante <= 100){
+    
+    var vlrParticipante = 100;
+   
+        if(numeroPalestrante <= vlrParticipante){
          
-            console.log("n° do Participante: " +numeroParticipante + " Permitir Cadastro !");
+            console.log(numeroParticipante + " ° Participante, seu nome é: " +nomeParticipante + ", Cadastro realizado!");
         }else{
        
             console.log(numeroParticipante + " Cadastro EXCEDIDO para Participante !");
@@ -47,4 +54,3 @@ while (inicio < cadastros) {
         console.log("-----------------------------------------");
         inicio++;        
     } 
-           
